7b34d0826293b6a4edaf98edd19aa376f5e403f0
Sam Webb <sam@samwebb.dev>
2025-06-15 00:32:48 +0100

    Import utils and add product db

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from models.products import Product
 from utils.data_reader import get_products
+from utils.db_tools import fill_db
+from models.db import db
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite3"
-db = SQLAlchemy(app)
-products = get_products()
+db.init_app(app)
+
+with app.app_context():
+    fill_db(get_products(), Product)
 
 
 @app.route("/")
 def route_index():
+    products = Product.query.all()
     return render_template("index.html", products=products)
+
+
+@app.route("/product/<productId>")
+def route_product(productId):
+    products = Product.query.all()
+    return render_template("product.html", product=products[productId])
 
 
 if __name__ == "__main__":

30a0e2bd8f4e738eeacaad295e8b31088bb59e0b
Sam Webb <sam@samwebb.dev>
2025-06-14 18:57:17 +0100

    Formatting

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from utils.data_reader import get_products
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite3:///data.sqlite3"
-db = SQLAlchemy(app) 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite3"
+db = SQLAlchemy(app)
+products = get_products()
 
-@app.route('/')
+
+@app.route("/")
 def route_index():
-    return render_template("index.html")
+    return render_template("index.html", products=products)
+
 
 if __name__ == "__main__":
     app.run(debug=True)

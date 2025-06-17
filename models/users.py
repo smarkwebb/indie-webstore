a28e4ddec7b56115880faaea58eb27f60752acb8
Sam Webb <sam@samwebb.dev>
2025-06-17 20:02:29 +0100

    Move class to db

--- models/users.py
+++
@@ -1,7 +0,0 @@
-from models.db import db
-
-
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    username = db.Column(db.String, nullable=False)
